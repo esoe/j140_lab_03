f26d308da600dd49647637d79aa95e7630586311
esoe <igor_nikulin@mail.ru>
2023-02-25 19:27:43 +0300

    qq

---
+++ src/main/java/ru/molokoin/LoginController.java
@@ -0,0 +1,12 @@
+package ru.molokoin;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class LoginController {
+    @FXML
+    private void login() throws IOException {
+        System.out.println("Инициирована авторизация пользователя ...");
+    }
+    
+}
