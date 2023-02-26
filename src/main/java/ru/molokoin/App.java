983beb1292e1c3eda2cfed5426af8b95fcafd4c0
esoe <igor_nikulin@mail.ru>
2023-02-26 22:53:08 +0300

    qq

--- src/main/java/ru/molokoin/App.java
+++ src/main/java/ru/molokoin/App.java
@@ -5,21 +5,38 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ru.molokoin.login.Frame;
 
 import java.io.IOException;
 
 /**
- * JavaFX App
+ * Класс,запускающий работу приложения
+ * -запуск сцены login
  */
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("Авторизация");
-        stage.setScene(scene);
-        stage.show();
+        // new Frame().init();
+        stage = new Frame();
+        ((Frame) stage).init();
+        // Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        // Scene scene = new Scene(root);
+        // stage.setTitle("Авторизация");
+        // stage.setScene(scene);
+        // stage.show();
     }
+    @Override
+    public void init() throws IOException {
+        // new Frame().init();
+        // stage = new Frame();
+        // ((Frame) stage).init();
+        // Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        // Scene scene = new Scene(root);
+        // stage.setTitle("Авторизация");
+        // stage.setScene(scene);
+        // stage.show();
+    }
+
     public static void main(String[] args) {
         launch();
     }
